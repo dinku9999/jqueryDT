4454c0f9fb2b589b366b24926a4ceb40962ccb41
dinku9999 <anup.vk29@gmail.com>
2016-09-26 15:30:16 -0400

    Update rowRemover.js

--- rowRemover.js
+++ rowRemover.js
@@ -1,4 +1,4 @@
-function tableCleaner(tableName){    // for example first 3 column data in the row
+function tableCleaner(tableName){    // for example , if first three elements in row are empty delete row
 			$('#'+tableName+' tbody tr').each(function() {
 			    if($(this)[0].childNodes[0].innerText==""||$(this)[0].childNodes[0].innerText ==" " || $(this)[0].childNodes[0].innerText==null
 			    	&& $$(this)[0].childNodes[1].innerHTML=="" || $(this)[0].childNodes[1].innerHTML==" " || $(this)[0].childNodes[1].innerHTML==null
@@ -8,11 +8,10 @@ function tableCleaner(tableName){    // for example first 3 column data in the r
 			    }
 			});
 }
-// The above function can be called in this case. You can always customize it depending on 
+// The above function can be called in this case. You can always customize it dependingly
+
 // considering that there are 5 columns in a datatable , and you want to remove a row if first 3 elements in the datatable are empty or null 
-
-
-function tableCleanerForOneRow(tableName){    // for example 3rd column data in the row
+function tableCleanerForOneRow(tableName){    // for example , if first 3rd element in row is empty/null delete row
 			$('#'+tableName+' tbody tr').each(function() {
 			    if($(this)[0].childNodes[2].innerHTML==""||$(this)[0].childNodes[2].innerHTML ==" " || $(this)[0].childNodes[2].innerHTML==null){
 			    	$(this).remove();
